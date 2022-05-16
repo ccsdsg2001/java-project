2605f62af20ba78e4c5cd5477fb2f9a30ed90329
ccsdsg2001 <ccsdsg2016@qq.com>
2022-05-16 22:54:10 +0800

    thread safety

--- OOP/src/test1/windowtest.java
+++ OOP/src/test1/windowtest.java
@@ -5,42 +5,74 @@ package test1;
  * @date 2022年05月15日 13:13
  */
 
- class window extends Thread{
+ class window implements Runnable {
 
-    private static int ticket =100;
+    private static int ticket = 100;
+    Object object = new Object();
     @Override
-    public void run() {
-        while (true){
-            if(ticket>0){
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println(getName()+"号为"+ticket);
-                ticket--;
-            }else{
-                break;
-            }
+    public synchronized void run() {
+        while (true) {
+            show();
         }
     }
+
+    public void show() {
+        while (true) {
+//            synchronized (object) {//synchronized (this){错误的
+                if (ticket > 0) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName() + "号为" + ticket);
+                    ticket--;
+                } else {
+                    break;
+                }
+            }
+        }
+
+
+
+
 }
+//        while (true) {
+//            synchronized (object) {//synchronized (this){错误的
+//                if (ticket > 0) {
+//                    try {
+//                        Thread.sleep(100);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                    System.out.println(Thread.currentThread().getName() + "号为" + ticket);
+//                    ticket--;
+//                } else {
+//                    break;
+//                }
+//            }
+//        }
+//    }
+//}
+
+
 
 public class windowtest {
     public static void main(String[] args) {
-
-
         window window1 = new window();
         window window2 = new window();
         window window3 = new window();
+//       Thread thread =new Thread(window1);
+//       Thread thread2 =new Thread(window1);
+//       Thread thread3 =new Thread(window1);
 
-        window1.setName("窜口");
-        window2.setName("窜口2");
-        window3.setName("窜口3");
 
-        window1.start();
-        window2.start();
-        window3.start();
-
+//       window1.setName("1");
+//       window2.setName("2");
+//       window3.setName("3");
+//
+//       window1.start();
+//       window2.start();
+//       window3.start();
+        }
     }
-}
