1cc1af1a9851bb74076b0bf34ad761ca74cb8618
ccsdsg2001 <ccsdsg2016@qq.com>
2022-05-22 00:02:22 +0800

    sTRNIG

---
+++ OOP/src/StringTest.java
@@ -0,0 +1,18 @@
+/**
+ * @author cc
+ * @date 2022年05月21日 23:44
+ */
+
+public class StringTest {
+
+
+    public void test1(){
+        String s1="avc";
+        String s2="avc";
+        s1="hello";
+
+    }
+
+}
+
+
--- OOP/src/java1/threadpool.java
+++ OOP/src/java1/threadpool.java
@@ -14,6 +14,7 @@ class NumberThread implements Runnable{
         for(int i =0;i<100;i++){
             if(i%2==0){
                 System.out.println(i);
+
             }
         }
     }
