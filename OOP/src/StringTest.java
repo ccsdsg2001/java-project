417e6b8b55975d80abf2d46e87e4dad54cca94c4
ccsdsg2001 <ccsdsg2016@qq.com>
2022-05-22 15:01:28 +0800

    String

--- OOP/src/StringTest.java
+++ OOP/src/StringTest.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * @author cc
  * @date 2022年05月21日 23:44
@@ -6,10 +8,44 @@
 public class StringTest {
 
 
-    public void test1(){
-        String s1="avc";
-        String s2="avc";
-        s1="hello";
+    public static void main(String[] args) {
+        String s1="hello World";
+        System.out.println(s1.length());
+        System.out.println(s1.charAt(2));
+        System.out.println(s1.isEmpty());
+        String s2=s1.toLowerCase();
+        System.out.println(s2.toLowerCase());
+        String s3="  dada dasds das   ";
+        System.out.println(s3.trim());
+        System.out.println(s1.equalsIgnoreCase(s2));
+        String s5="af";
+        String s6=s3.concat(s5);
+        System.out.println(s6);
+        System.out.println(s5.compareTo(s3));
+        System.out.println(s1.substring(1,3));
+        System.out.println(s1.endsWith("ld"));
+        System.out.println(s1.startsWith("he"));
+        System.out.println(s1.startsWith("11",2));
+        System.out.println(s1.contains(s2));
+        System.out.println(s1.indexOf("h"));
+        System.out.println(s1.indexOf("h",5));
+        System.out.println(s1.lastIndexOf("h",5));
+        System.out.println(s1.replace("h", "w"));
+        System.out.println(s1.matches("^h|l$"));
+
+        String s22="123";
+        int num=Integer.parseInt(s22);
+        System.out.println(num);
+
+        String srt1="abc123";
+        char[] chars = srt1.toCharArray();
+        for(char chars1:chars){
+            System.out.println(chars1);
+        }
+
+        String st1="ada中国";
+        byte[] bytes=st1.getBytes();
+        System.out.println(Arrays.toString(bytes));
 
     }
 
