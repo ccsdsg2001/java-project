cd722659fdc9dacb2465e474d51ad090acfff7de
ccsdsg2001 <ccsdsg2016@qq.com>
2022-05-30 23:26:51 +0800

    ComparbleTest

--- OOP/src/ComparbleTest.java
+++ OOP/src/ComparbleTest.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * @author cc
@@ -9,11 +10,24 @@ public class ComparbleTest implements Comparable {
         String[] cc=new String[]{"AA","BB","FF"};
 
         Arrays.sort(cc);
+        Arrays.sort(cc, new Comparator<>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return 0;
+            }
 
+            @Override
+            public boolean equals(Object obj) {
+                return false;
+            }
+        });
     }
 
     @Override
     public int compareTo(Object o) {
         return 0;
     }
+
+
+
 }
