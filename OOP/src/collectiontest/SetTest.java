ded548c4dfcf25a2dbb8c3662f5ab0f3cec36704
ccsdsg2001 <ccsdsg2016@qq.com>
2022-06-10 19:08:00 +0800

    SET

--- OOP/src/collectiontest/SetTest.java
+++ OOP/src/collectiontest/SetTest.java
@@ -2,9 +2,7 @@ package collectiontest;
 
 import org.junit.Test;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author cc
@@ -24,5 +22,60 @@ public class SetTest {
         while (iterator.hasNext()){
             System.out.println(iterator.next());
         }
+    }
+
+    @Test
+    public void test2(){
+        Set set=new LinkedHashSet();
+        set.add(123);
+        set.add(423);
+        set.add("AA");
+        set.add(1239);
+        set.add(123);
+
+        Iterator iterator = set.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+    @Test
+    public void test14(){
+        TreeSet set = new TreeSet();
+        set.add(123);
+        set.add(342);
+        set.add(3);
+        set.add(1232);
+
+        Iterator iterator = set.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+
+    @Test
+    public void test1(){
+        Comparator comparator = new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return 0;
+            }
+        };
+        TreeSet set = new TreeSet(comparator);
+        set.add(123);
+        set.add(342);
+        set.add(3);
+        set.add(1232);
+
+        Iterator iterator = set.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+
+
+    @Override
+    public int hashCode() {
+//        int result
+        return super.hashCode();
     }
 }
