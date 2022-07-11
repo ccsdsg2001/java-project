83f95d5e34696a3e96834b32d113960bf502445f
ccsdsg2001 <ccsdsg2016@qq.com>
2022-07-12 03:09:04 +0800

    网络编程

---
+++ Reflaction/Person.java
@@ -0,0 +1,52 @@
+/**
+ * @author cc
+ * @date 2022年07月12日 2:49
+ */
+public class Person {
+    private String name;
+    public int Age;
+
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        Age = age;
+    }
+    private Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", Age=" + Age +
+                '}';
+    }
+
+    public void show(){
+        System.out.println("w");
+    }
+
+    private void AT(){
+        System.out.println("private");
+    }
+}
---
+++ Reflaction/ReflectionTest.java
@@ -0,0 +1,58 @@
+import org.junit.Test;
+
+import javax.print.DocFlavor;
+import java.lang.reflect.*;
+
+/**
+ * @author cc
+ * @date 2022年07月12日 2:51
+ */
+public class ReflectionTest {
+    @Test
+    public void  TEST(){
+        Person p1=new Person("to", 1);
+
+        p1.Age=10;
+        System.out.println(p1.toString());
+        p1.show();
+
+
+//        在外部,不能通过对象去调用私有结构,以及私有的构造器
+    }
+
+    @Test
+    public void test2() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchFieldException {
+        Class clazz=Person.class;
+//        1.通过反射,创建Person类的对象
+        Constructor constructor = clazz.getConstructor(String.class, int.class);
+        Object tom = constructor.newInstance("Tom", 12);
+        Person person =(Person) tom;
+
+        System.out.println(person.toString());
+//      2.通过反射,调用对象的指定属性和方法
+        Field name = clazz.getDeclaredField("name");
+        name.set("pe", 10);
+
+        Method show = clazz.getDeclaredMethod("show");
+        show.invoke("p");
+//        通过反射,可以调用Person类的私有结构,如方法,属性
+
+        Constructor declaredConstructor = clazz.getDeclaredConstructor(String.class);
+        declaredConstructor.setAccessible(true);
+        Person person1 = (Person) declaredConstructor.newInstance("Jerry");
+        System.out.println(person1);
+
+//        调用私有属性
+        Field name3=clazz.getDeclaredField("name");
+        name3.setAccessible(true);
+        name3.set(person1, "jmmm");
+        System.out.println(person1);
+
+//
+//        调用私有方法
+        Method at = clazz.getDeclaredMethod("AT", DocFlavor.STRING.class);
+        at.setAccessible(true);
+        at.invoke(person1,"zhj")
+
+    }
+}
